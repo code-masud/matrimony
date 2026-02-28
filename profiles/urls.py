4f0408cf3aad851c9687b44ff241102eb65e5917
code-masud <code.masud@hotmail.com>
2026-02-28 18:58:44 +0600

    chat

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name='profiles'
 urlpatterns = [
     path('', ProfileView.as_view(), name="my_profile"),
+    path('chat/', ChatView.as_view(), name="chat"),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -11,4 +11,13 @@ class ProfileView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Profile'
         return context
+
+class ChatView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
+    template_name = 'profile/chat.html'
+    permission_required = ['profile.view_matrimony_profile']
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Chat'
+        return context
     
