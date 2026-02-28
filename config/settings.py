7ed7e37f488dc645ddc507df9684fc6095b7a7b1
code-masud <code.masud@hotmail.com>
2026-02-28 14:39:29 +0600

    company

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrap5',
+    'company.apps.CompanyConfig',
     'accounts.apps.AccountsConfig',
     'profiles.apps.ProfilesConfig',
     'matches.apps.MatchesConfig',
