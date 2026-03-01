b6ee5c6378de1bfc86460d6c6b96d22d560d0c3f
code-masud <code.masud@hotmail.com>
2026-03-01 14:38:49 +0600

    update

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
 class ProfileView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
@@ -10,5 +13,14 @@ class ProfileView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Profile'
+        return context
+
+class ProfileDetail(DetailView):
+    model = User
+    template_name = 'profile/detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = f'Detail - {self.get_object().username}'
         return context
     
