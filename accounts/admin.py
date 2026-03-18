cd24e4712dda31b8b5ca9d57cad99c54bc07e357
code-masud <code.masud@hotmail.com>
2026-03-19 01:27:14 +0600

    update

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,90 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    model = User
+
+    list_display = (
+        'id',
+        'username',
+        'phone',
+        'email',
+        'on_behalf',
+        'is_verified',
+        'is_staff',
+        'is_active',
+        'date_joined',
+    )
+    list_display_links = ('id', 'username', 'phone', 'email')
+    list_filter = (
+        'is_verified',
+        'is_staff',
+        'is_active',
+        'on_behalf',
+        'date_joined',
+    )
+
+    search_fields = (
+        'username',
+        'phone',
+        'email',
+    )
+
+    ordering = ('-date_joined',)
+
+    readonly_fields = ('date_joined', 'last_login')
+
+    fieldsets = (
+        (None, {
+            'fields': ('username', 'password')
+        }),
+        (_('Personal Info'), {
+            'fields': ('first_name', 'last_name', 'email', 'phone')
+        }),
+        (_('On Behalf Info'), {
+            'fields': ('on_behalf',)
+        }),
+        (_('Verification'), {
+            'fields': ('is_verified',)
+        }),
+        (_('Permissions'), {
+            'fields': (
+                'is_active',
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            ),
+        }),
+        (_('Important Dates'), {
+            'fields': ('last_login', 'date_joined'),
+        }),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'username',
+                'phone',
+                'email',
+                'password1',
+                'password2',
+                'on_behalf',
+                'is_verified',
+                'is_staff',
+                'is_active',
+            ),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions')
+
+    def save_model(self, request, obj, form, change):
+        if not obj.pk:
+            if obj.is_staff:
+                obj.is_verified = True
+        super().save_model(request, obj, form, change)
--- chat/views.py
+++ chat/views.py
@@ -1,13 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 # Create your views here.
+
+
 class ChatView(LoginRequiredMixin, PermissionRequiredMixin, TemplateView):
     template_name = 'chat/room.html'
     permission_required = ['chat.view_chat']
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        users = User.objects.exclude(id=self.request.user.id)
         context["title"] = 'Chat'
+        context["users"] = users
         return context
