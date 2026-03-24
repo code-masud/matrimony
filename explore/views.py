7b1e7102ba10d1ec64ee97301fbcbfbea5521391
code-masud <code.masud@hotmail.com>
2026-03-25 00:17:57 +0600

    update

--- explore/views.py
+++ explore/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.contrib.auth import get_user_model
+from cities_light.models import Country
+from django.db.models import Q
+from datetime import date, timedelta
 
 User = get_user_model()
 
@@ -10,8 +13,50 @@ class ExploreView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = 'Search'
+        request = self.request
 
-        search_result = User.objects.exclude(id=self.request.user.id).all()
-        context['search_result'] = search_result
+        results = User.objects.exclude(
+            id=request.user.id).select_related('matrimony_profile')
+
+        query = request.GET.get('q', "")
+        religion = request.GET.get('religion', "")
+        location = request.GET.get('location', "")
+        age_range = request.GET.get('age_range', "")
+
+        # 3. Apply Filters
+        if query:
+            results = results.filter(
+                Q(username__icontains=query) |
+                Q(first_name__icontains=query) |
+                Q(last_name__icontains=query)
+            )
+
+        if religion:
+            results = results.filter(matrimony_profile__religion=religion)
+
+        if location:
+            results = results.filter(matrimony_profile__country_id=location)
+
+        if age_range:
+
+            try:
+                min_age, max_age = map(int, age_range.split('-'))
+                today = date.today()
+
+                results = results.filter(
+                    matrimony_profile__date_of_birth__range=[
+                        today - timedelta(days=365.25 * max_age),
+                        today - timedelta(days=365.25 * min_age)
+                    ]
+                )
+            except ValueError:
+                pass
+
+        context["query"] = query
+        context["age_range"] = age_range
+        context["location"] = location
+        context["religion"] = religion
+        context["title"] = 'Search'
+        context['countries'] = Country.objects.all()
+        context['search_result'] = results
         return context
