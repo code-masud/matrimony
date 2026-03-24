e07387d45f052b95744fea87c2bd61e31d9f43c3
code-masud <code.masud@hotmail.com>
2026-03-24 18:06:15 +0600

    search

--- explore/views.py
+++ explore/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+
+
 class ExploreView(TemplateView):
     template_name = 'explore/index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Search'
+
+        search_result = User.objects.exclude(id=self.request.user.id).all()
+        context['search_result'] = search_result
         return context
--- profiles/models.py
+++ profiles/models.py
@@ -1,9 +1,10 @@
+from datetime import date
 from django.db import models
 from django.conf import settings
 from services.uploads import avatar_upload_path, gallery_image_upload_path
 from services.validations import image_validation
 
-# Create your models here.
+
 class MatrimonyProfile(models.Model):
 
     class GenderChoices(models.TextChoices):
@@ -79,6 +80,14 @@ class MatrimonyProfile(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    @property
+    def age(self):
+        if not self.date_of_birth:
+            return None
+        today = date.today()
+        not_yet_reached = (today.month, today.day) < (self.date_of_birth.month, self.date_of_birth.day)
+        return today.year - self.date_of_birth.year - not_yet_reached
+    
     class Meta:
         ordering = ["-created_at"]
         indexes = [
