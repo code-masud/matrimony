d1058c5d426e5424d2352fb9c816ce4dfaee0671
code-masud <code.masud@hotmail.com>
2026-04-03 22:53:49 +0600

    explore

--- matches/urls.py
+++ matches/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import *
 
-app_name='matches'
+app_name = 'matches'
 urlpatterns = [
     path('', MatchesView.as_view(), name='my_match'),
     path('shortlist/', ShortListView.as_view(), name='my_shortlist'),
+    path('send-interest/', send_interest, name='send_interest'),
 ]
--- matches/views.py
+++ matches/views.py
@@ -1,8 +1,20 @@
+from email import message
+
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.views.generic import TemplateView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+
+from django.http import JsonResponse
+from .models import InterestRequest
+from notifications.models import Notification
+
+User = get_user_model()
 
 # Create your views here.
+
+
 class MatchesView(TemplateView):
     template_name = 'matches/index.html'
 
@@ -11,6 +23,7 @@ class MatchesView(TemplateView):
         context["title"] = 'Matches'
         return context
 
+
 class ShortListView(TemplateView):
     template_name = 'matches/shortlist.html'
 
@@ -18,3 +31,41 @@ class ShortListView(TemplateView):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Shortlist'
         return context
+
+
+@login_required
+def send_interest(request):
+    receiver_id = request.POST.get('receiver_id')
+
+    if not receiver_id:
+        return JsonResponse({"error": "receiver_id missing"}, status=400)
+
+    receiver = User.objects.get(id=receiver_id)
+
+    interest, created = InterestRequest.objects.get_or_create(
+        sender=request.user,
+        receiver=receiver,
+        defaults={  
+            "status": InterestRequest.StatusChoices.PENDING,
+            "message": f"{request.user.username} sent you an interest"
+        }
+    )
+
+    if created:
+        Notification.objects.create(
+            sender=request.user,
+            receiver=receiver,
+            notification_type="interest_sent",
+            text=f"{request.user.username} sent you an interest"
+        )
+
+        return JsonResponse({
+            "status": "success",
+            "message": "Interest sent successfully"
+        })
+
+    else:
+        return JsonResponse({
+            "status": "exists",
+            "message": "You already sent interest"
+        }, status=200)
