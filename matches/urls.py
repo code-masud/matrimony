8df61fc66870bc2075d5c2fcd766f41e0c7e1d3a
code-masud <code.masud@hotmail.com>
2026-04-03 23:34:53 +0600

    Update urls.py

--- matches/urls.py
+++ matches/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', MatchesView.as_view(), name='my_match'),
     path('shortlist/', ShortListView.as_view(), name='my_shortlist'),
     path('send-interest/', send_interest, name='send_interest'),
+    path('make-shortlist/', make_shortlist, name='make_shortlist'),
 ]
