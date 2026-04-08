de5ca6fb3b173b19b58c1aa9a6375e2151cc304e
code-masud <code.masud@hotmail.com>
2026-04-08 19:39:50 +0600

    Update admin.py

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Message
 
 # Register your models here.
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    model = Message
+
+    list_display = (
+        'sender',
+        'receiver',
+        'content',
+        'is_delivered',
+        'is_seen',
+    )
+
+    list_per_page = 10
