79f9b0d579d35a0a74dfdb607dd80f6da450c711
code-masud <code.masud@hotmail.com>
2026-04-08 17:00:18 +0600

    Update admin.py

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from .models import MatrimonyProfile, PartnerPreference, ProfilePhoto
+from django.contrib.auth import get_user_model
 
-# Register your models here.
+User = get_user_model()
+
+
+@admin.register(MatrimonyProfile)
+class MatrimonyProfileAdmin(admin.ModelAdmin):
+    model = MatrimonyProfile
+
+    list_display = (
+        'user__username',
+        'gender',
+        'date_of_birth',
+        'height_cm',
+        'marital_status',
+        'religion',
+        'mother_tongue',
+        'education',
+        'occupation',
+        'annual_income',
+        'country',
+        'is_profile_completed',
+    )
+
+    list_display_link = (
+        'user__username',
+        'gender',
+        'date_of_birth',
+        'height_cm',
+        'marital_status',
+        'religion',
+        'mother_tongue',
+        'education',
+        'occupation',
+        'annual_income',
+        'country',
+        'is_profile_completed',
+    )
+    list_per_page = 10
