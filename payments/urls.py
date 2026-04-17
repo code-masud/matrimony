df2e7f974dc11964fd69387521696dcfa4a4b450
code-masud <code.masud@hotmail.com>
2026-04-17 20:44:27 +0600

    update

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from .views import *
 
-app_name='payments'
+app_name = 'payments'
 urlpatterns = [
     path('', PaymentView.as_view(), name='my_payment'),
-    path('checkout/', PaymentView.as_view(), name='checkout'),
+    path('checkout/', CheckoutView.as_view(), name='checkout'),
+    path('success/', SuccessView.as_view(), name='success'),
+    path('cancel/', CancelView.as_view(), name='cancel'),
 ]
--- payments/views.py
+++ payments/views.py
@@ -11,9 +11,25 @@ class PaymentView(TemplateView):
         return context
 
 class CheckoutView(TemplateView):
-    template_name = 'payments/terms_conditions.html'
+    template_name = 'payments/checkout.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["title"] = 'Terms & Conditions'
+        context["title"] = 'Checkout'
+        return context
+    
+class SuccessView(TemplateView):
+    template_name = 'payments/success.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Success'
+        return context
+    
+class CancelView(TemplateView):
+    template_name = 'payments/cancel.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = 'Cancel'
         return context
