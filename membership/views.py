27bf6fc48868b35dc6bb1f5a3c2142ad6d51714a
code-masud <code.masud@hotmail.com>
2026-04-18 13:40:16 +0600

    updaate

--- membership/views.py
+++ membership/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView
 
-# Create your views here.
-class MembershipView(TemplateView):
+from membership.models import Membership
+
+
+class MembershipView(ListView):
     template_name = 'membership/index.html'
+    model = Membership
+    context_object_name = 'memberships'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["title"] = 'Membership'
         return context
-    
